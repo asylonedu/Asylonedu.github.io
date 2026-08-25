72b4ba9584626feee59438dbed99545db94fa0ad
asylonedu <luciana.orellana@asylonedu.com>
2026-08-25 14:40:31 +0000

    img7

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,6 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection,image } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+import { image } from "astro:content";
 
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
